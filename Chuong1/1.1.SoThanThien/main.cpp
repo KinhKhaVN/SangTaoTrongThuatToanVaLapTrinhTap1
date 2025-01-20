6ac301587b06efa04423e1913690ed03d9dde97c
KinhKhaVN <pebin0123@gmail.com>
2025-01-20 16:29:29 +0700

    First commit Chapter 1

---
+++ Chuong1/1.1.SoThanThien/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	int a;
+	std::cout << "Hay nhap 1 so: ";
+	std::cin >> a;
+	
+	std::cout << "So do la: " << a;
+	
+	return 0;
+}
